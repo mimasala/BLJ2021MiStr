2a52c68bcf51d7807fa7050c53aac27120a75596
mimasala <mytallmusic@gmail.com>
2021-10-06 13:18:11 +0200

    new repo

---
+++ C-Aufgaben/cmake-build-debug/CMakeFiles/C_Aufgaben.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Aufgaben.dir/main.c.obj"
+  "C_Aufgaben.exe"
+  "C_Aufgaben.exe.manifest"
+  "C_Aufgaben.pdb"
+  "libC_Aufgaben.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Aufgaben.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
