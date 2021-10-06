c871738921f64ef20aad7aa897701cc45faabe47
mimasala <mytallmusic@gmail.com>
2021-10-06 13:54:54 +0200

    added 2.2

--- C-Aufgaben/cmake-build-debug/CMakeFiles/C_Aufgaben.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for C_Aufgaben.
-# This may be replaced when dependencies are built.
--- C-Aufgaben/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    printf("hello World!\n");
-    printf("it doesnt work Sadge\n")
-
-    return 0;
-}
