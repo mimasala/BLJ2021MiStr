b2b3f1d69f10b4f690867a04f3837632e94d8032
mimasala <mytallmusic@gmail.com>
2021-10-08 16:38:59 +0200

    started 624

--- C-projects/624/main.c
+++ C-projects/624/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    int i = 0;
+    char z;
+    char n[] = {0};
+    const char s[2] = " ";
+    char *token;
+
+    printf("bitte geben sie ein zeichen ein, welches für die anzeige verwendet wird");
+    scanf("%c",z);
+    printf("Bitte geben Sie die Diagrammwerte ein (durch Leerschlag getrennt):");
+    scanf("%s",n);
+
+    while( token != NULL) {
+        printf( " %s\n", token );
+        token = strtok(NULL, s);
+        i++;
+    }
+
+    for (int i = 0; i < n; ++i) {
+
+
+    }
+
+
 }
