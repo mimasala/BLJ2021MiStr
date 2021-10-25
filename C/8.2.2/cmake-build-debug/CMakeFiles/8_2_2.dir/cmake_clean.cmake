8c272a06ebece1ffef2c776118f6c56db579e603
mimasala <mytallmusic@gmail.com>
2021-10-25 14:14:33 +0200

    push

---
+++ C/8.2.2/cmake-build-debug/CMakeFiles/8_2_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "8_2_2.exe"
+  "8_2_2.exe.manifest"
+  "8_2_2.pdb"
+  "CMakeFiles/8_2_2.dir/main.c.obj"
+  "lib8_2_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/8_2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/9.2.1/cmake-build-debug/CMakeFiles/9_2_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9_2_1.exe"
+  "9_2_1.exe.manifest"
+  "9_2_1.pdb"
+  "CMakeFiles/9_2_1.dir/main.c.obj"
+  "lib9_2_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/9_2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
