faa66481d37b88e74606d67e06ff0bf3781ff06a
mimasala <65661176+mimasala@users.noreply.github.com>
2021-10-28 17:29:40 +0200

    started 12

--- C/12.2.1/main.c
+++ C/12.2.1/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-// function prototype
-int convert(long long n) {
+int bin2dec(long long *p_n ) {
     int dec = 0;
 
     return dec;
 }
 
 int main() {
-    long long n;
+    long long n[256];
     printf("Enter a binary number: ");
     scanf("%lld", &n);
-    printf("%lld in binary = %d in decimal", n, convert(n));
+    printf("%lld in binary = %d in decimal", n, bin2dec(n));
     return 0;
 }
--- C/13.2.1/main.c
+++ C/13.2.1/main.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+double calcPoints(double a, double b, double xmin, double xmax, double *xcon[100], double *ycon[100]);
 
 int main() {
 
-    long long plane[100][100]={0};
+    double a;
+    double b;
+    double xmin;
+    double xmax;
+    double xcon[100];
+    double ycon[100];
+    int p_xcon;
+    int p_ycon;
 
+    printf("Bitte die Steigung eingeben:\n");
+    scanf("%lf", &a);
+    printf("Bitte den Offset eingeben:\n");
+    scanf("%lf", &b);
+    printf("Bitte den Xmin eingeben:\n");
+    scanf("%lf", &xmin);
+    printf("Bitte den Xmax eingeben:\n");
+    scanf("%lf", &xmax);
+
+    calcPoints(a, b, xmin, xmax, &p_xcon, &p_ycon);
+   
+    for (int i = 0; i <= 100; ++i) {
+        printf("Zahl %d: %lf", i, xcon[i]);
+    }
 
     return 0;
+}
+
+double calcPoints(double a, double b, double xmin, double xmax, double *xcon[100], double *ycon[100]){
+
+    for (int i = 0; i  < 100 ; i = i+1) {
+        if (xmin <= xmax){
+            *xcon[i] = a*xmin + b;
+            *ycon[i] = xmin;
+            xmin = xmin + 1;
+        }
+
+
+    }
+
+
 }
